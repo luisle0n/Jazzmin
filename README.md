964a227df836ff5d1bf750ca7ccbc185617e03fb
luisle0n <lfleon9@utpl.edu.ec>
2025-06-17 09:45:48 -0500

    Pagina con las configuraciones de jazzmin

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Jazzmin
 Proyecto hecho con django y luego puestas las Configurationes de jazzmin 
+
+
+## Proyecto antes de usar las configuraciones de jazzmin
+
+![alt text](image.png)
+
+
+## Proyecto depues de usar las configuraciones de jazzmin
+
+![alt text](image-1.png)
---
+++ semana3/practica/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from practica.models import *
+# Register your models here.
+
+
+class InscripcionInline(admin.TabularInline):
+    model = Inscripcion
+    extra = 1
+    verbose_name = "Inscripción"
+    verbose_name_plural = "Inscripciones"
+
+class CusroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'departamento', 'instructor')
+    list_filter = ('departamento', 'instructor')
+    search_fields = ('titulo',)
+    inlines = [InscripcionInline]
+
+class EntregaInline(admin.TabularInline):
+    model = Entrega
+    extra = 1
+    verbose_name = "Entrega"
+    verbose_name_plural = "Entregas"
+
+class TareaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'curso', 'fecha_entrega')
+    inlines = [EntregaInline]
+
+
+admin.site.register(Departamento)
+admin.site.register(Instructor)
+admin.site.register(Curso, CusroAdmin)
+admin.site.register(Estudiante)
+admin.site.register(Inscripcion)
+admin.site.register(Tarea)
+
+admin.site.register(Entrega)
